6a98b435cb8ab97e88f6f2c1d1c4918e74f48c6c
Jordan Facibene <jordan@networktocode.com>
2017-06-28 10:53:40 -0400

    commit cable_box module

---
+++ cable_box.py
@@ -0,0 +1,13 @@
+
+
+class Box:
+
+	def __init__(self, box_name, box_serial, box_resolution, box_type, box_space):
+		self.name = box_name
+		self.serial = box_serial
+		self.resolution = box_resolution
+		self.type = box_type
+		self.space = box_space
+
+
+
